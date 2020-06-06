6b6f43af4efa2aeea80aed419de68ce6d4d4d850
tanya-555 <52540265+tanya-555@users.noreply.github.com>
2020-06-06 23:13:40 +0530

    add presenter for listing screen

---
+++ app/src/main/java/com/example/restaurantfinder/presenter/ListingPresenter.java
@@ -0,0 +1,55 @@
+package com.example.restaurantfinder.presenter;
+
+import com.android.volley.RequestQueue;
+import com.example.restaurantfinder.contract.ListingContract;
+import com.example.restaurantfinder.model.SearchInfoModel;
+import com.example.restaurantfinder.model.SearchResponse;
+
+import java.util.List;
+
+public class ListingPresenter extends ListingContract.Presenter implements ListingContract.Model.onFinishedListener {
+
+    private ListingContract.View attachedView;
+    private SearchInfoModel model;
+
+    public ListingPresenter() {
+        model = new SearchInfoModel();
+    }
+
+    @Override
+    public void onFinished(List<SearchResponse> collectionList) {
+        if(attachedView != null) {
+            attachedView.setData(collectionList);
+            attachedView.showContent();
+        }
+    }
+
+    @Override
+    public void onFailed(Throwable t) {
+        if(attachedView != null) {
+            attachedView.showError(t, false);
+        }
+    }
+
+    @Override
+    public void fetchCollections(RequestQueue queue, int cityId, int collectionId, String order) {
+        attachedView.showLoading(false);
+        callApi(queue, cityId, collectionId, order);
+    }
+
+    @Override
+    public void attachView(ListingContract.View view) {
+        super.attachView(view);
+        attachedView = view;
+    }
+
+    @Override
+    public void detachView() {
+        super.detachView();
+        attachedView = null;
+    }
+
+    private void callApi(RequestQueue queue, int cityId, int collectionId, String order) {
+        model.getData(queue, cityId, collectionId, order, this);
+    }
+}
