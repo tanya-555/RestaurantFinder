fdab0c3ff1dcee1ca58b698e746b3e929a0efff7
tanya-555 <52540265+tanya-555@users.noreply.github.com>
2020-06-07 20:39:51 +0530

    add event for sort option selected

---
+++ app/src/main/java/com/example/restaurantfinder/event/SortOptionSelectedEvent.java
@@ -0,0 +1,9 @@
+package com.example.restaurantfinder.event;
+
+public class SortOptionSelectedEvent {
+    public String sortOption;
+
+    public SortOptionSelectedEvent(String sortOption) {
+        this.sortOption = sortOption;
+    }
+}
